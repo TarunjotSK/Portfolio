03e26b7cf80474d41edeb8d6eb1379f991fc81f6
TarunjotSK <tarunjot30@gmail.com>
2024-10-19 17:04:44 -0400

    About Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
     </div>
     <ComputersCanvas/>
 
-    <div className='absolute w-full flex justify-center items-center'>
+    <div className='absolute w-full  bottom-5 flex justify-center items-center'>
     <a href='#about'>
     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
     <motion.div
