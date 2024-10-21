667da6600e7d4d5b4810a9264da9e5520c9b5e98
TarunjotSK <tarunjot30@gmail.com>
2024-10-21 00:22:40 -0400

    Computer Changes 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
     </div>
     <ComputersCanvas/>
 
-    <div className={`absolute w-full ${isMobile ? 'bottom-40' : 'bottom-10'} flex justify-center items-center`}>
+    <div className={`absolute w-full ${isMobile ? 'bottom-40' : 'bottom-0'} flex justify-center items-center`}>
     <a href='#about'>
     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
     <motion.div
