02ddd48c28cf5c13af2c078c58805f1cf3805330
TarunjotSK <tarunjot30@gmail.com>
2024-11-09 19:01:09 -0500

    Changes for emailjs6

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () =>{
   return (
     <BrowserRouter>
      <div className="realtive z-0 bg-primary">
-     <p>NODE_ENV value is "{JSON.stringify(process.env.NODE_ENV)}"</p>
+     <p>NODE_ENV value is "{JSON.stringify(process)}"</p>
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
